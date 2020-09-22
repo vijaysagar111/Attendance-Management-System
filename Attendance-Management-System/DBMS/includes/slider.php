009320ca99a6b42e2667898fb99b5bb550a6465b
vijay sagar gooty <31399807+vijaysagar111@users.noreply.github.com>
2020-09-22 15:20:45 +0530

    Add files via upload

---
+++ Attendance-Management-System/DBMS/includes/slider.php
@@ -0,0 +1,8 @@
+ <div>
+            <!-- Carousel -->
+            <div id="templatemo-carousel" class="carousel slide" data-ride="carousel">
+                <!-- Indicators -->
+                	<img src="image/gvpt.jpg">
+
+            </div><!-- /#templatemo-carousel -->
+        </div>
